c0c64b0cb43f690f8d1261b47ffa6896eabf07e5
NArunkumar <nelluri.arunkumar@gmail.com>
2025-10-15 19:10:16 +0530

    Working on EKS upgrade stratagies (Blue-Green)

--- 80-eks/main.tf
+++ 80-eks/main.tf
@@ -3,7 +3,7 @@ module "eks" {
     version = "~> 21.0" # this is module version
 
     name = "${var.project}-${var.environment}"
-    kubernetes_version = "1.32"
+    kubernetes_version = "1.33"
 
     addons = {
         coredns = {}
@@ -34,7 +34,24 @@ module "eks" {
 
     # EKS Managed Node Group(s)
     eks_managed_node_groups = {
-        blue = {
+        # blue = {
+        #     # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
+        #     ami_type = "AL2023_x86_64_STANDARD" # user name is ec2-user
+        #     instance_type = ["m5.xlarge"]
+
+        #     min_size = 2
+        #     max_sixe = 10
+        #     desired_size = 2
+
+        # }
+
+        # iam_role_additional_policies = {
+        #     AmazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+        #     AmazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+        #     AmazonEKSLoad = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
+        # }
+
+        green = {
             # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
             ami_type = "AL2023_x86_64_STANDARD" # user name is ec2-user
             instance_type = ["m5.xlarge"]
@@ -43,13 +60,21 @@ module "eks" {
             max_sixe = 10
             desired_size = 2
 
-        }
+            iam_role_additional_policies = {
+                AmazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+                AmazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+                AmazonEKSLoad = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
+            }
 
-        # iam_role_additional_policies = {
-        #     AmazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-        #     AmazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
-        #     AmazonEKSLoad = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
-        # }
+            taints = {
+                upgrade = {
+                    key = "upgrade"
+                    value = "true"
+                    effect = "NO_SCHEDULE"
+                }
+            }
+
+        }
 
 
     }
