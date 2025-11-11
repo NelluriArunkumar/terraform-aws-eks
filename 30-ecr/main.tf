90579e69e27a82448ee34e85bec4a19db6a78748
NArunkumar <nelluri.arunkumar@gmail.com>
2025-11-11 11:23:23 +0530

    Adding ECR and IAM policy terraform files

---
+++ 30-ecr/main.tf
@@ -0,0 +1,12 @@
+resource "aws_ecr_repository" "Foo" {
+    count = length(var.images)
+    name = "${var.project}/${var.images[count.index]}"
+    image_tag_mutability = "MUTABLE"
+
+    image_scanning_configuration {
+      scan_on_push = true
+    }
+
+    force_delete = true
+  
+}
---
+++ 30-ecr/variables.tf
@@ -0,0 +1,12 @@
+variable "project" {
+    default = "roboshop"  
+}
+
+variable "environment" {
+    default = "dev"
+  
+}
+
+variable "images" {
+    default = ["catalogue", "user", "cart", "shipping", "payment", "frontend"]  
+}
