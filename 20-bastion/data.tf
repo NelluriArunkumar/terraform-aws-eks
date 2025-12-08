92ce29d1931106ae1f5f085d260ea49016888b14
NArunkumar <nelluri.arunkumar@gmail.com>
2025-12-08 11:29:00 +0530

    Terraform files for Roboshop Infra

--- 20-bastion/data.tf
+++ 20-bastion/data.tf
@@ -6,6 +6,15 @@ data "aws_ami" "joindevops" {
       name = "name"
       values = ["RHEL-9-DevOps-Practice"]
     }
+    filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
 data "aws_ssm_parameter" "bastion_sg_id" {
